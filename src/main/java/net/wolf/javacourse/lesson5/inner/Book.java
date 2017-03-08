cb6a4de38ce81fa240d78d4466f2e1fa53caac0c
Andrey <Andrey@DESKTOP-HQ696A6>
2017-03-08 22:18:26 +0200

    Примеры для курса Java

---
+++ src/main/java/net/wolf/javacourse/lesson5/inner/Book.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.wolf.javacourse.lesson5.inner;
+
+/**
+ *
+ * @author Andrey
+ */
+public class Book {
+
+        private final String name;
+        private final String author;
+        private final int year;
+        private final Publisher publisher;
+
+        public Book(String name, String author, int year, String publ) {
+
+                this.name = name;
+                this.author = author;
+                this.year = year;
+                publisher = new Publisher(publ);
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public String getAuthor() {
+                return author;
+        }
+
+        public int getYear() {
+                return year;
+        }
+
+        public Publisher getPublisher() {
+                return publisher;
+        }
+
+        public class Publisher {
+
+                public String name;
+                public Book book;
+
+                Publisher(String name) {
+                        book = Book.this;
+                        this.name = name;
+                }
+
+                public String getName() {
+                        return name;
+                }
+
+                public Book getBook() {
+                        return book;
+                }
+                
+                public String getAuthor() {
+                        return author;
+                }
+
+        }
+        
+        public static void main(String[] args) {
+                 Book book = new Book("Меч Предназначения", "Анджей Сапковский", 1992, "АСТ, Полиграфиздат");
+        }
+}
---
+++ src/main/java/net/wolf/javacourse/lesson5/inner/Question.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.wolf.javacourse.lesson5.inner;
+
+/**
+ * В вопросе "Вопрос" понятие "типа вопроса" (class Type) является очевидным. 
+ * Альтернатива - создать класс верхнего уровня QuestionType, - будет менее выразительной, 
+ * по крайней мере в контексте класса Question.
+ * @author Andrey
+ */
+public class Question {
+
+        private Type type;
+
+        public Question(Type type) {
+                this.type = type;
+        }
+
+        public Type getType() {
+                return type;
+        }
+
+        public void setType(Type type) {
+                this.type = type;
+        }
+
+        public static enum Type {
+                SINGLE_CHOICE, MULIT_CHOICE, TEXT
+        }
+        
+        public static void main(String[] args) {
+                 Question question = new Question(Type.TEXT);
+                 
+                if (question.getType() == Question.Type.TEXT) { 
+                        System.out.println("Вопрос требующий ответа в виде текста!");
+                }
+        }
+}
